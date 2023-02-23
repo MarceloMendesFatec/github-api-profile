78cef766330c32582ab8b0c8483b5b71d370c37a
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2023-02-23 15:30:47 -0300

    refatoramento de codigo

--- script.js
+++ script.js
@@ -4,15 +4,33 @@ function search() {
     var url = `https://api.github.com/users/${userName}`;
 
     $.getJSON(url, (user) => {
-        document.getElementById("name").innerHTML = user.name;
-        document.getElementById("company").innerHTML = user.company;
-        document.getElementById("html_url").innerHTML = user.html_url;
-        document.getElementById("avatar_url").innerHTML = `
-         <img  src=${user.avatar_url} class= "shadow rounded " 
-          width="200px" height="200px"> `;
+      setDataUser(user);
+      clearError();
 
     }).fail( () => {
-
-        document.getElementById("error").innerHTML = "<div class='alert alert-danger' 'role=alert'> Não Encontrado ! </div>";
+       setDataUser({});
+        showError();
     });
 }
+
+
+
+
+function showError(){
+    document.getElementById("error").innerHTML = "<div class='alert alert-danger' 'role=alert'> Nao Encontrado ! </div>";
+
+};
+
+function clearError(){
+    document.getElementById("error").innerHTML = "";
+};
+
+
+function setDataUser(user){ 
+    document.getElementById("name").innerHTML = user.name || "";
+    document.getElementById("company").innerHTML = user.company || "";
+    document.getElementById("html_url").innerHTML = user.html_url || "";
+    document.getElementById("avatar_url").innerHTML = user.avatar_url ? 
+                                                   ` <img  src=${user.avatar_url} class= "shadow rounded " 
+                                                    width="200px" height="200px"> ` : "";
+};
